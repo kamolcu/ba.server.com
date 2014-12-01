de2486e86315bb02c2e850acf019dcbb7791969d
kamol <kamolcu@gmail.com>
2014-12-01 11:57:20 +0700

    Add more debug logs and prevent Undefined index: data

--- app/models/PCMSManager.php
+++ app/models/PCMSManager.php
@@ -12,6 +12,8 @@ class PCMSManager
         '8' => 'Counter Service',
     );
     public function updateData($model, $data, $datasetId) {
+        $msg = sprintf('PCMSManager::updateData datasetId = %s, model = %s, data = %s', $datasetId, $model, print_r($data, true));
+        Log::debug($msg);
         foreach ($data as $name => $count) {
             $row_exists = App::make('Helper')->isCompletedOrderExists($name, $datasetId);
             if (!$row_exists) {
@@ -44,7 +46,7 @@ class PCMSManager
             return $dataSet;
         }
         catch(Exception $ex) {
-            $msg = sprintf('Exception = %s', $ex->getMessage());
+            $msg = sprintf('getPaidOrder Exception = %s', $ex->getMessage());
             Log::debug($msg);
             return null;
         }
@@ -66,7 +68,7 @@ class PCMSManager
             return $dataSet;
         }
         catch(Exception $ex) {
-            $msg = sprintf('Exception = %s', $ex->getMessage());
+            $msg = sprintf('getCompletedOrder Exception = %s', $ex->getMessage());
             Log::debug($msg);
             return null;
         }
@@ -83,17 +85,22 @@ class PCMSManager
         }
 
         $client = new GuzzleHttp\Client();
-        $response = $client->post(Config::get('config.' . App::environment() . '.pcms_api_url') , array(
+        $uri = Config::get('config.' . App::environment() . '.pcms_api_url');
+        $response = $client->post($uri, array(
             'body' => array(
                 'start_date' => $start_date,
                 'end_date' => $end_date
             )
         ));
+        $msg = sprintf('Post to PCMS with %s', $uri);
+        Log::debug($msg);
         $orders = array();
         $json_result = $response->getBody()->getContents();
         if (!empty($json_result)) {
             $output = json_decode($json_result, true);
-            $orders = $output['data'];
+            if (isset($output['data'])) {
+                $orders = $output['data'];
+            }
         }
         //$json_result = '{"status":"success","code":200,"message":"Successfully get order by period 2014-02-08 00:00:00 to 2014-02-08 23:59:59","data":[{"id":12150,"payment_method":"1","order_status":"new","payment_status":"failed","total_price":24000},{"id":12151,"payment_method":"8","order_status":"new","payment_status":"waiting","total_price":3900},{"id":12153,"payment_method":"8","order_status":"new","payment_status":"waiting","total_price":3900},{"id":12154,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":20000},{"id":12155,"payment_method":"8","order_status":"new","payment_status":"waiting","total_price":3900},{"id":12157,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":8900},{"id":12158,"payment_method":"1","order_status":"new","payment_status":"reconcile","total_price":3700},{"id":12160,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":8900},{"id":12161,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":8900},{"id":12163,"payment_method":"1","order_status":"new","payment_status":"reconcile","total_price":8900},{"id":12169,"payment_method":"1","order_status":"new","payment_status":"reconcile","total_price":17000},{"id":12174,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":9000},{"id":12176,"payment_method":"8","order_status":"new","payment_status":"reconcile","total_price":1700},{"id":12177,"payment_method":"5","order_status":"new","payment_status":"reconcile","total_price":22000},{"id":12180,"payment_method":"1","order_status":"expired","payment_status":"expired","total_price":80000},{"id":12182,"payment_method":"1","order_status":"new","payment_status":"reconcile","total_price":20000},{"id":12184,"payment_method":"5","order_status":"new","payment_status":"waiting","total_price":240},{"id":12185,"payment_method":"8","order_status":"new","payment_status":"reconcile","total_price":440},{"id":12189,"payment_method":"1","order_status":"new","payment_status":"reconcile","total_price":420}],"count":19}';
         return $orders;
